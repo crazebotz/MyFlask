328f81017790e391677ac1452b5ac3d1e6b6b5b0
crazebotz <120011436+crazebotz@users.noreply.github.com>
2024-08-18 09:05:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
-git clone https://github.com/crazebotz/MyFlask ok && cd ok && pip3 install -U -r requirements.txt
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/crazebotz/MyFlask.git
+
+# Navigate into the repository directory
+cd MyFlask
+
+# Install dependencies from requirements.txt
+pip3 install -U -r requirements.txt
+
+# Start the Flask application
 python app.py
